3ebbd4a6e23071d1675934677324638cdf1a8014
ruchikamattoo <ruchika.mattoo@essi.co.in>
2018-10-05 13:56:50 +0530

    server setup

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+console.log('sc')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+// specify start of page
+app.get('/', (req, res) => {
+  res.send({
+    name: 'ruchika',
+    likes: ['h', 'd']
+  })
+})
+app.listen(process.env.PORT || 8081)
